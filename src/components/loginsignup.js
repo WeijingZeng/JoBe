e5b4a5fb1154ab6da095636c75d1b5232cb2c765
Patrick Hill <graffixnyc@gmail.com>
2017-11-11 23:50:41 -0500

    more login/signup prep

---
+++ src/components/loginsignup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+  class LoginSignUp extends Component {
+    render() {
+        return (
+            <div >
+            LOGIN/SIGNUP
+            </div>
+        )
+    }
+}
+
+export default LoginSignUp;
