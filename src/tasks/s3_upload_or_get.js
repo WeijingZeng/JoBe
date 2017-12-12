0ec6db04482d8faac34e88066a1c155a22b89871
Yugaank Sharma <yugaanks@yahoo.co.in>
2017-12-12 17:55:28 -0500

    profile pictures are saved to s3 buckets when user logs in via facebook or google

--- src/tasks/s3_upload_or_get.js
+++ src/tasks/s3_upload_or_get.js
@@ -1,7 +1,13 @@
+// function calls are at the end of the file
 var AWS = require('aws-sdk');
 var fs = require('fs');
+var request = require('request');
 
-AWS.config.loadFromPath('config.json');
+//cannot load config from path, giving AWS.FileWriteStream is not a constructor, so hard coded the values below.
+//AWS.config.loadFromPath('config.json');
+AWS.config.region='us-west-2'
+AWS.config.accessKeyId="AKIAIRQKEHCLTSW4LKCQ"
+AWS.config.secretAccessKey="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"
 
 var s3 = new AWS.S3();
 
@@ -31,10 +37,62 @@ function uploadFile(remoteFilename, fileName) {
 	});
 }
 
+//upload profile pictures from facebook and google to s3... need to set a default suffix or prefix with filenames
+// this will not work as I was downloading the image and then trying to upload, but client(browser) doesn't understand readFile or writeFileStream
+function uploadFileFromURL(remoteFilename, fileName) {
+  	 download(fileName, remoteFilename, function(){			//download(uri, new_file_name, callback)
+ 		var fileBuffer = fs.readFileSync('google.jpg');
+	  	var metaData = 'image/jpg';
+	  
+		s3.putObject({
+		    ACL: 'public-read',
+		    Bucket: 'imagemagick-jobe',
+		    Key: remoteFilename,
+		    Body: fileBuffer,
+		    ContentType: metaData
+		  }, function(error, response) {
+			    console.log('uploaded file: '+fileName);
+		});
+	});
+	
+}
+
+// Working for google right now
+function put_from_url(url, filename, callback) {
+    request({
+        url: url,
+        encoding: null
+    }, function(err, res, body) {
+        if (err)
+            return callback(err, res);
+
+        s3.putObject({
+        	ACL: 'public-read',
+            Bucket: 'imagemagick-jobe',
+            Key: 'images/'+filename,
+            ContentType: res.headers['content-type'],
+            ContentLength: res.headers['content-length'],
+            Body: body // buffer
+        }, function(error, response) {
+			    console.log('uploaded file');
+		});
+    })
+}
+
+//download the file from remote to local 
+ function download(uri, filename, callback){			// download(uri, new_file_name, callback)
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+
 /*
 * Get a file from s3
 */
-
 function getFile(fileName){
 	var urlParams = {Bucket: 'imagemagick-jobe', Key: 'images/'+fileName};
 	//signed url as a temporary url pointing to that image
@@ -43,5 +101,11 @@ function getFile(fileName){
 	});
 }
 
+//uploadFileFromURL('images/google.jpg', 'https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg')
 //uploadFile('images/band.jpg', 'band.jpg');
-getFile('band.jpg');
+//getFile('band.jpg');
+
+export function test(url, filename) {
+	//uploadFileFromURL("google.jpg" ,url);
+	put_from_url(url, filename)
+}
