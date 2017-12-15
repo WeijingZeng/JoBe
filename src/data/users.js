4763a3f149303a31b9b0816bf79e6fa1590a6ca2
Patrick Hill <graffixnyc@gmail.com>
2017-12-15 16:27:59 -0500

    Edit User Function

--- src/data/users.js
+++ src/data/users.js
@@ -170,6 +170,80 @@ let exportedMethods = {
             return this.getUserById(addedUser.insertedId);
         }
     },
+    async editUser(
+        firebaseID,
+        username,
+        firstName,
+        lastName,
+        email,
+        gender,
+        city,
+        state,
+        age,
+        long,
+        lat,
+        seeking,
+        studioSWUsed,
+        mainGenre,
+        secondGenre,
+        thirdGenre,
+        hasSpace,
+        bio,
+        achivements,
+        role,
+        links,
+        influences,
+        lastLogin,
+        profilePhotoUrl,
+        localRemoteOrAll,
+        distanceIfLocal
+    ) {
+        //need error checking here to make sure all fields are supplied and also need to check that their handle is unique
+        let userCollection = await users();
+        //here we check if the username(handle) supplied is unique if it's not, then there
+        // will be an error returned and if it is then it goes ahead and adds the user
+        let updatedUser = {
+                _id:firebaseID,
+                username: username.toLowerCase(),
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                gender: gender,
+                city: city,
+                state: state,
+                age: age,
+                location: { type: "Point", coordinates: [long, lat] },
+                seeking: seeking,
+                studioSWUsed: studioSWUsed,
+                mainGenre: mainGenre,
+                secondGenre: secondGenre,
+                thirdGenre: thirdGenre,
+                hasSpace: hasSpace,
+                bio: bio,
+                achivements: achivements,
+                role: role,
+                links: links,
+                influences: influences,
+                matchingActive: 1,
+                lastLogin: lastLogin,
+                profilePhotoUrl: profilePhotoUrl,
+                profileViewCount: 0,
+                adminUser: 0,
+                localRemoteOrAll: localRemoteOrAll,
+                distanceIfLocal: distanceIfLocal
+        };
+        let updateCommand = {
+            $set: updatedUser
+        };
+        
+        let editedUser= userCollection.updateOne({ _id: firebaseID }, updateCommand).then(() => {
+            return this.getUserById(firebaseID);
+        });
+            
+        
+
+        
+    },
     async removeUser(id) {
         return users().then(userCollection => {
             return userCollection.removeOne({ _id: id }).then(deletionInfo => {
