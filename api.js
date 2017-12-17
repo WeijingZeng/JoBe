57fa3240328d2756e1c7bd944ab4d08b5ca24342
Brandon Rothweiler <brandonrothweiler@gmail.com>
2017-12-17 14:11:04 -0500

    add express.json in api.js

--- api.js
+++ api.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const configAPIRoutes = require("./api/index");
 
-
-console.log("config routes api.js "+JSON.stringify(configAPIRoutes));
 app.use(express.urlencoded());
+app.use(express.json());
 configAPIRoutes(app);
 
 app.listen(4000, () => {  
